80af32ab4a972af40063464a65d786e89b687a0b
Gyu Jeong Kim <gjkim@beyless.com>
2022-10-26 16:57:49 +0900

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="name">{{name}}</div>
+  <div>{{greeting('JeongEEE')}}</div>
+  <div>{{greet}}</div>
+  <div>{{name}}</div>
+	<button class="btn btn-primary" @click="updateName">Click</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from 'vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  setup() {
+		const name = ref('JeongEEE');
+
+		const greeting = (name) => {
+			return 'Hello, ' + name
+		};
+
+		const greet = greeting(name.value);
+
+		const updateName = () => {
+			name.value = 'GyuJeong'
+		}
+
+		return {
+			name, greeting, greet, updateName
+		};
+	}
 }
 </script>
 
@@ -22,5 +37,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.name {
+	color: blue;
 }
 </style>
