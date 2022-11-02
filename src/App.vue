92ae747fc88e3d8a691fde3b6d8c3f13b818b63e
Gyu Jeong Kim <gjkim@beyless.com>
2022-11-02 16:03:30 +0900

    Search

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
 	<div class="container">
-		<h4>count : {{ count }}</h4>
-		<h4>double count computed : {{ doubleCountComputed }}</h4>
-		<h4>double count method: {{ doubleCountMethod() }}</h4>
-		<button @click="count++">Add One</button>
 		<h2>To-Do List</h2>
+		<input class="form-control" type="type" v-model="searchText" 
+			placeholder="Search">
+		<hr />
 		<TodoSimpleForm @add-todo="addTodo" />
-		<div v-if="!todos.length">
-			추가된 Todo가 없습니다
+		<div v-if="!filteredTodos.length">
+			There is nothing to display
 		</div>
-		<TodoList :todos="todos" @toggle-todo="toggleTodo"
+		<TodoList :todos="filteredTodos" @toggle-todo="toggleTodo"
 			@delete-todo="deleteTodo" />
 	</div>
 </template>
@@ -44,19 +43,20 @@ export default {
 		function toggleTodo(index) {
 			todos.value[index].completed = !todos.value[index].completed;
 		}
-
-		const count = ref(1);
-		const doubleCountComputed = computed(() => {
-			return count.value * 2;
-		});
-
-		const doubleCountMethod = () => {
-			return count.value * 2;
-		}
+		
+		const searchText = ref('');
+		const filteredTodos = computed(() => {
+			if(searchText.value) {
+				return todos.value.filter(todo => {
+					return todo.subject.includes(searchText.value);
+				})
+			}
+			return todos.value;
+		})
 
 		return {
 			todos, addTodo, updateName, todoStyle, deleteTodo, toggleTodo,
-			count, doubleCountComputed, doubleCountMethod
+			searchText, filteredTodos
 		};
 	}
 }
